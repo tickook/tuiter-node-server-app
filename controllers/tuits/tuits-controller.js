6a440fea410dd397a1da63e5dad365c1edef3a4e
kartiktickoo <tickoo.k@northeastern.edu>
2022-11-20 20:26:27 -0500

    first commit

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,45 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    // const newTuit = tuits[0];
+    const newTuit = {...tuits[0], ...req.body};
+    // console.log(req.body);
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    newTuit.handle = "kartiktickoo";
+    newTuit.userName = "KartikTickoo"
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
+const findTuits = (req, res) =>
+    res.json(tuits);
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id === tuitdIdToUpdate)
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+
+
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter((t) =>
+                             t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
+}
+
+
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
